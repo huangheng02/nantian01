1399b65cf6ad0989818d15de16d824ae760434f7
Administrator <Administrator@PC-201807041617>
2018-07-23 17:37:01 +0800

    请求

---
+++ src/main/java/com/demo/test/service/StudentService.java
@@ -0,0 +1,12 @@
+package com.demo.test.service;
+
+import com.demo.test.model.Student;
+
+public interface StudentService {
+	
+	public Student query(Student stu);
+	
+	public void list(int id);
+
+	
+}
