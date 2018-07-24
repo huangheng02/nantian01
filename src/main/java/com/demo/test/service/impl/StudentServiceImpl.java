1323e6a546c364bc8ca4b9b03a1aa9740a6c8d5e
Administrator <Administrator@PC-201807041617>
2018-07-24 09:46:20 +0800

    student实现类

---
+++ src/main/java/com/demo/test/service/impl/StudentServiceImpl.java
@@ -0,0 +1,20 @@
+package com.demo.test.service.impl;
+
+import com.demo.test.model.Student;
+import com.demo.test.service.StudentService;
+
+public class StudentServiceImpl implements StudentService {
+
+	@Override
+	public Student query(Student stu) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void list(int id) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
