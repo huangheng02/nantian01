6f29f44b219232c223740ca7efd10576463dde88
Administrator <Administrator@huangheng>
2018-08-10 16:40:31 +0800

    11

--- src/main/java/com/demo/test/model/Report.java
+++ src/main/java/com/demo/test/model/Report.java
@@ -3,4 +3,54 @@ package com.demo.test.model;
 public class Report {
 
 	private String test;
+	
+	int id1;
+	
+	int id2;
+	
+	int id3;
+	
+	int id4;
+
+	public String getTest() {
+		return test;
+	}
+
+	public void setTest(String test) {
+		this.test = test;
+	}
+
+	public int getId1() {
+		return id1;
+	}
+
+	public void setId1(int id1) {
+		this.id1 = id1;
+	}
+
+	public int getId2() {
+		return id2;
+	}
+
+	public void setId2(int id2) {
+		this.id2 = id2;
+	}
+
+	public int getId3() {
+		return id3;
+	}
+
+	public void setId3(int id3) {
+		this.id3 = id3;
+	}
+
+	public int getId4() {
+		return id4;
+	}
+
+	public void setId4(int id4) {
+		this.id4 = id4;
+	}
+	
+	
 }
