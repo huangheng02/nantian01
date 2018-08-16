b8eade9a52c03fd0ffe5a6d794822c0fc4af7f23
Administrator <Administrator@huangheng>
2018-08-16 17:32:08 +0800

    信息

---
+++ src/main/java/com/test/service/impl/TestServiceImpl.java
@@ -0,0 +1,14 @@
+package com.test.service.impl;
+
+import com.test.service.TestService;
+
+public class TestServiceImpl implements TestService {
+
+	@Override
+	public void test1() {
+		// TODO Auto-generated method stub
+
+		System.out.println("1");
+	}
+
+}
